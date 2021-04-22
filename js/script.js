bf6a5f2fe75b1e034ffbe99c3c3db493175db93c
krzwier <krzwier@gmail.com>
2021-04-21 23:43:18 -0500

    Create entry page and user welcome message

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
+var synth = window.speechSynthesis;
+
 var button = document.querySelector("button");
+var userName = document.querySelector("input");
+var p = document.querySelector("p");
 var modal = document.querySelector(".modal");
 var span = document.querySelector(".modal-x");
 
 button.addEventListener("click", function () {
-  button.innerText = "You've got this!";
-  modal.classList.add("show-modal");
+    if (userName.value === "Josh") {
+        welcome(userName.value);
+    }
+    button.innerText = "You've got this!";
+    modal.classList.add("show-modal");
 });
 
 span.addEventListener("click", function () {
-  button.innerText = "Who's got this?";
-  modal.classList.remove("show-modal");
+    button.innerText = "Who's got this?";
+    modal.classList.remove("show-modal");
 });
+
+var welcome = function (name) {
+    p.innerText = `Welcome, ${name}`;
+    var utterThis = new SpeechSynthesisUtterance(`Welcome, ${userName.value}!`);
+    synth.speak(utterThis);
+
+    
+}
